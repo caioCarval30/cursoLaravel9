4f0ee56a201f63f645cf341ec228b93d5e8b878b
Caio Sarmento <caio.sarmento@prodepa.pa.gov.br>
2022-09-05 08:29:36 -0300

    NEW: created blades

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', 'Listagem dos Usuários')
+
+@section('content')
+<h1>Listagem dos usuários
+    <a href="{{ route('users.create') }}">+</a>
+</h1>
+
+<ul>
+    @foreach ($users as $user)
+        <li>
+            {{ $user->name }} -
+            {{ $user->email }}
+            | <a href="{{ route('users.show', $user->id) }}">Detalhes</a>
+            | <a href="{{ route('users.edit', $user->id) }}">Editar</a>
+        </li>
+    @endforeach
+</ul>
+@endsection
